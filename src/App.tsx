6a480d523f2548205854ed233b95aec50446718b
ewigd13 <ewig.d13@gmail.com>
2025-09-26 13:14:33 +0900

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
-import CameraPage from './CameraPage';
-import SavedPage from './SavedPage';
-
 function App() {
-  const [savedImages, setSavedImages] = useState<string[]>([]);
-
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <CameraPage
-              savedImages={savedImages}
-              setSavedImages={setSavedImages}
-            />
-          }
-        />
-        <Route
-          path="/saved"
-          element={<SavedPage savedImages={savedImages} />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <div style={{ padding: '2rem' }}>
+      <h1>表示テスト</h1>
+      <p>このページが表示されれば描画は成功です。</p>
+    </div>
   );
 }
 
